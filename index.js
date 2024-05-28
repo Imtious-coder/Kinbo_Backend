980d73913f485cd256ff51ecb26a6d3eaccef697
Imtious-Coder <iarafat386@gmail.com>
2024-05-28 14:48:48 +0600

    :art: Image upload functionality added.

--- index.js
+++ index.js
@@ -16,8 +16,29 @@ mongoose.connect("mongodb+srv://kinbo:kinbo@cluster0.azdemqf.mongodb.net/kinbo")
 // API
 app.get("/", (req, res) => {
   res.send("Backend is running perfeclty.")
-})
+});
 
+// Image storing functionality 
+const storage = multer.diskStorage({
+  destination: "./Images",
+  filename: (req, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+});
+
+const upload = multer({ storage: storage});
+
+// Creating upload enpoint for images
+app.use("/images", express.static("./Images"));
+
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success : 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
+  })
+});
+
+// APP
 app.listen(port, (err) => {
   if(!err) {
     console.log("Server is running on port " + port)

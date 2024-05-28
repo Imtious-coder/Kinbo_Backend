25dad8a4445ad158e3ac8c86b4b36a60ae1c3995
Imtious-Coder <iarafat386@gmail.com>
2024-05-28 16:39:12 +0600

    :art:  Data added to mongoDB.

--- index.js
+++ index.js
@@ -11,34 +11,31 @@ app.use(express.json());
 app.use(cors());
 
 // Database connection with mongoDB
-mongoose.connect("mongodb+srv://kinbo:kinbo@cluster0.azdemqf.mongodb.net/kinbo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb+srv://kinbo:kinbo@cluster0.azdemqf.mongodb.net/kinbo")
 
 // API
 app.get("/", (req, res) => {
-  res.send("Backend is running perfectly.");
+  res.send("Backend is running perfeclty.")
 });
 
 // Image storing functionality 
 const storage = multer.diskStorage({
   destination: "./Images",
   filename: (req, file, cb) => {
-    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage});
 
-// Creating upload endpoint for images
+// Creating upload enpoint for images
 app.use("/images", express.static("./Images"));
 
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
-    success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
-  });
+    success : 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
+  })
 });
 
 // Schema for creating products.
@@ -75,35 +72,40 @@ const Product = mongoose.model("Product", {
     type: Boolean,
     default: true,
   },
-});
+})
 
 app.post("/addproduct", async (req, res) => {
-  console.log(req.body); // Log the request body to inspect incoming data
-
+  let products = await Product.find({});
+  let id;
+  if(products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
   const product = new Product({
-    id: req.body.id,
+    id: id,
     name: req.body.name,
     image: req.body.image,
     category: req.body.category,
     new_price: req.body.new_price,
     old_price: req.body.old_price,
   });
-
   console.log(product, "Created product");
   await product.save();
   console.log("Product Saved");
-
   res.json({
     success: true,
     name: req.body.name,
-  });
+  })
 });
 
 // APP
 app.listen(port, (err) => {
-  if (!err) {
-    console.log("Server is running on port " + port);
+  if(!err) {
+    console.log("Server is running on port " + port)
   } else {
-    console.log("Something is wrong" + err);
+    console.log("Something is wrong" + err)
   }
 });
